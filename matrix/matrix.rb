d8831c4003e6cafe635bac8535ee715098698472
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-20 20:42:09 -0600

    Create matrix class

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,8 +1,32 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+require 'pry'
+class Matrix
+  attr_reader :string
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+  def initialize(string)
+    @string = string
+  end
 
+  def rows
+    rows = []
+    split = @string.split("\n")
+    split.each do |row|
+      rows << row.split.map(&:to_i)
+    end
+    rows
+  end
+
+  def columns
+    columns = {}
+    rows.map do |row|
+      row.each_with_index do |int, index|
+        if columns[index]
+          columns[index] << int
+        else
+          columns[index] = [int]
+        end
+      end
+    end
+    columns
+  end
+
+end
