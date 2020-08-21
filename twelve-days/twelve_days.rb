f9538fceef267bba0b11d401401b24fbc3e38f4a
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-21 12:37:00 -0600

    Add pseudocode and test

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -1,8 +1,27 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
+require 'pry'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
+class TwelveDays
 
+  def self.song
+    # instantiate Verse by reading txt file
+    # save verses to instance variable as hash with digit as key
+    # access line using digit
+    # iterate adding one line each rotation
+  end
+
+  def self.verses
+    @verses = {}
+    verses = ["verse 1", "verse 2", "verse 3"]
+    verse_number = 1
+    verses.map do |verse|
+      @verses[verse_number] = verse
+      verse_number += 1
+    end
+    @verses
+  end
+end
+
+# class Verse
+#   def initialize(verse)
+#   end
+# end
