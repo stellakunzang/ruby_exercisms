01d28528d708dacbfa94271df7c01e22ed483ffa
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-23 21:50:57 -0600

    First iteration of twelve days program

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -2,26 +2,39 @@ require 'pry'
 
 class TwelveDays
 
+  @days = {"first" => 1, "second" => 2, "third" => 3, "fourth" => 4, "fifth" => 5, "sixth" => 6, "seventh" => 7, "eighth" => 8, "ninth" => 9, "tenth" => 10, "eleventh" => 11, "twelfth" => 12}
+  @gifts = ["a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds", "five Gold Rings", "six Geese-a-Laying", "seven Swans-a-Swimming", "eight Maids-a-Milking", "nine Ladies Dancing", "ten Lords-a-Leaping", "eleven Pipers Piping", "twelve Drummers Drumming"]
+
   def self.song
-    # instantiate Verse by reading txt file
-    # save verses to instance variable as hash with digit as key
-    # access line using digit
-    # iterate adding one line each rotation
+    song = ""
+    @days.each do |day, digit|
+      song += make_song(day, digit)
+    end
+    song
   end
 
-  def self.verses
-    @verses = {}
-    verses = ["verse 1", "verse 2", "verse 3"]
-    verse_number = 1
-    verses.map do |verse|
-      @verses[verse_number] = verse
-      verse_number += 1
+  def self.make_song(day, digit)
+    verse = "On the " + day + " day of Christmas my true love gave to me: "
+    gifts = reverse_gifts(digit)
+    if day != "first" && day != "twelfth"
+      verse += pile_gifts(gifts) + double_line_break
+    elsif day == "first"
+      verse += @gifts.first + "." + double_line_break
+    else
+      verse += pile_gifts(gifts) + "\n"
     end
-    @verses
+    verse
+  end
+
+  def self.reverse_gifts(digit)
+    @gifts[0..(digit - 1)].reverse
+  end
+
+  def self.pile_gifts(gifts)
+    gifts[0..-2].join(', ') + ", and " + gifts[-1] + '.'
+  end
+
+  def self.double_line_break
+    "\n" + "\n"
   end
 end
-
-# class Verse
-#   def initialize(verse)
-#   end
-# end
--- twelve-days/twelve_days_test.rb
+++ twelve-days/twelve_days_test.rb
@@ -3,16 +3,9 @@ require_relative 'twelve_days'
 
 class TwelveDaysTest < Minitest::Test
   def test_the_whole_song
-    skip
+    # skip
     song_file = File.expand_path('../song.txt', __FILE__)
     expected = IO.read(song_file)
     assert_equal expected, TwelveDays.song
   end
-
-  def test_versification
-    assert_equal Hash, TwelveDays.verses.class
-  end
-
-
-
 end
